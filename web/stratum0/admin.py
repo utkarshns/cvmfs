2f691229461ea41efa00e7a6925a127ac6dd3a03
Rene Meusel <rene@renemeusel.de>
2013-11-06 16:01:44 +0000

    indentation using 4 spaces (django convention)

--- web/stratum0/admin.py
+++ web/stratum0/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from stratum0.models import Stratum1
 
 class Stratum1Admin(admin.ModelAdmin):
-	fields = ['url']
-	list_display = ['url', 'stratum0_fqrn']
-	list_filter = ['stratum0_fqrn']
+    fields       = ['url']
+    list_display = ['url', 'stratum0_fqrn']
+    list_filter  = ['stratum0_fqrn']
 
 admin.site.register(Stratum1, Stratum1Admin)
--- web/stratum0/models.py
+++ web/stratum0/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Stratum1(models.Model):
-	stratum0_fqrn = models.CharField('mirrored stratum 0 repo', max_length=100)
-	url           = models.CharField('stratum 1 URL',           max_length=255)
+    stratum0_fqrn = models.CharField('mirrored stratum 0 repo', max_length=100)
+    url           = models.CharField('stratum 1 URL',           max_length=255)
 
-	def __unicode__(self):
-		return self.url
+    def __unicode__(self):
+        return self.url
