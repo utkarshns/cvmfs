d305607415313ed7ac4a4c224435825477e9552b
Rene Meusel <rene@renemeusel.de>
2013-11-06 16:15:30 +0000

    create and wire up a second view

--- web/stratum0/urls.py
+++ web/stratum0/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from stratum0 import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<stratum1_id>\d+)/$', views.details, name='details'),
 )
--- web/stratum0/views.py
+++ web/stratum0/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World!")
+
+def details(request, stratum1_id):
+    return HttpResponse("You will look at: %s" % stratum1_id)
