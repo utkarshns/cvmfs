21ae7effa3482189ce9e3865dcd548342c141328
Rene Meusel <rene@renemeusel.de>
2013-11-06 14:55:28 +0000

    stub for Stratum1 database model

--- web/stratum0/models.py
+++ web/stratum0/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Stratum1(models.Model):
+	stratum0_fqrn = models.CharField('mirrored stratum 0 repo', max_length=100)
+	url           = models.CharField('stratum 1 URL',           max_length=255)
+
+	def __unicode__(self):
+		return self.url
