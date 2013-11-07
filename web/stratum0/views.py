859ef8aef84e2118dad2a585ece39723a479bc27
Rene Meusel <rene@renemeusel.de>
2013-11-07 16:14:12 +0000

    show additional information for remote stratum1 repositories

--- web/stratum0/views.py
+++ web/stratum0/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from stratum0.models import Stratum1
-import cvmfs.repository
+from cvmfs.repository import all_local_stratum0, RemoteRepository
 
 def index(request):
     stratum1_mapping = {
-        stratum0: [ stratum1 for stratum1 in
+        stratum0: [ RemoteRepository(stratum1.url) for stratum1 in
                     Stratum1.objects.filter(stratum0_fqrn=stratum0) ]
-        for stratum0 in cvmfs.repository.all_stratum0()
+        for stratum0 in all_local_stratum0()
     }
     context = {'stratum1_mapping': stratum1_mapping}
     return render(request, 'stratum0/index.html', context)
