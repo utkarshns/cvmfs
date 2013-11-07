adecdbc54cb3aab0696af385c15e73cdb28078e2
Rene Meusel <rene@renemeusel.de>
2013-11-07 14:53:27 +0000

    create a mapping from local stratum0 to remote stratum1

--- web/stratum0/views.py
+++ web/stratum0/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from stratum0.models import Stratum1
 import cvmfs.repository
 
 def index(request):
-    stratum0_list = cvmfs.repository.all_stratum0()
-    context = {'stratum0_list': stratum0_list}
+    stratum1_mapping = {
+        stratum0: [ stratum1 for stratum1 in
+                    Stratum1.objects.filter(stratum0_fqrn=stratum0) ]
+        for stratum0 in cvmfs.repository.all_stratum0()
+    }
+    context = {'stratum1_mapping': stratum1_mapping}
     return render(request, 'stratum0/index.html', context)
 
 def details(request, stratum1_id):
