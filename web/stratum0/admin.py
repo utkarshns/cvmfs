1b1f87f3c6d3d4226a1ebd32e2147f788e123591
Rene Meusel <rene@renemeusel.de>
2013-11-08 14:07:16 +0000

    add 'name' to the shown fields in the admin interface

--- web/stratum0/admin.py
+++ web/stratum0/admin.py
@@ -3,7 +3,7 @@ from stratum0.models import Stratum1
 
 class Stratum1Admin(admin.ModelAdmin):
     fields       = ['url']
-    list_display = ['url', 'stratum0_fqrn']
+    list_display = ['url', 'stratum0_fqrn', 'name']
     list_filter  = ['stratum0_fqrn']
 
 admin.site.register(Stratum1, Stratum1Admin)
