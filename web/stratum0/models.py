4ec838660003701b2cd4b22d44a42524bbb929d0
Rene Meusel <rene@renemeusel.de>
2013-11-08 09:01:12 +0000

    add 'name' to the Stratum1 model

--- web/stratum0/models.py
+++ web/stratum0/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Stratum1(models.Model):
     stratum0_fqrn = models.CharField('mirrored stratum 0 repo', max_length=100)
     url           = models.CharField('stratum 1 URL',           max_length=255)
+    name          = models.CharField('name of the replica',     max_length=100)
 
     def __unicode__(self):
         return self.url
