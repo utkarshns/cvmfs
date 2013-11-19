20aa3b1164e4c8e7dd7738e9da40c3fd843cc47c
Rene Meusel <rene@renemeusel.de>
2013-11-19 14:42:06 +0000

    add button for manual snapshot start

--- web/stratum0/urls.py
+++ web/stratum0/urls.py
@@ -5,4 +5,5 @@ from stratum0 import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^(?P<stratum0_fqrn>.+)/$', views.details, name='details'),
+    url(r'^(?P<stratum0_fqrn>.+)/replicate$', views.details, name='replicate'),
 )
