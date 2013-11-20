a32e279ece3747c92b13c428cae07da480874477
Rene Meusel <rene@renemeusel.de>
2013-11-20 16:04:12 +0000

    add stratum0 buisness logic to the replication button

--- web/stratum0/urls.py
+++ web/stratum0/urls.py
@@ -4,6 +4,6 @@ from stratum0 import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<stratum0_fqrn>.+)/replicate/(?P<stratum1_id>\d+)/$', views.StartSnapshotRedirectView.as_view(), name='replicate'),
     url(r'^(?P<stratum0_fqrn>.+)/$', views.details, name='details'),
-    url(r'^(?P<stratum0_fqrn>.+)/replicate$', views.details, name='replicate'),
 )
--- web/stratum0/views.py
+++ web/stratum0/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponse
+from django.views.generic.base import RedirectView
+from django.views.decorators.cache import never_cache
+from django.core.urlresolvers import reverse_lazy
 
 from stratum0.models import Stratum1
 import cvmfs.repository
@@ -23,3 +26,27 @@ def details(request, stratum0_fqrn):
         return render(request, 'stratum0/details.html', context)
     except cvmfs.repository.RepositoryNotFound, e:
         raise Http404
+
+
+class StartSnapshotRedirectView(RedirectView):
+    permanent=False
+
+    def get_redirect_url(self, *args, **kwargs):
+        stratum0_fqrn = kwargs['stratum0_fqrn']
+        stratum1_id   = kwargs['stratum1_id']
+        stratum1_info = get_object_or_404(Stratum1, pk=stratum1_id)
+
+        if stratum1_info.stratum0_fqrn != stratum0_fqrn:
+            raise Http404("FQRNs differ (%s : %s)" % (stratum1_info.stratum0_fqrn,
+                                                      stratum0_fqrn))
+
+        stratum1 = cvmfs.repository.RemoteRepository(stratum1_info.url)
+        if not stratum1.has_rest_api():
+            raise Http404("Stratum 1 at %s does not provide a REST API" % stratum1_info.url)
+
+        if stratum1.type != 'stratum1':
+            raise Http404("%s is not a Stratum 1" % stratum1_info.url)
+
+        stratum1.start_replication()
+        return reverse_lazy('details', kwargs={'stratum0_fqrn': stratum0_fqrn},
+                            current_app='stratum0')
