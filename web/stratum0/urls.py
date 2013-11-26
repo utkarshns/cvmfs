1724afc4bb382340f37e36346ac2fa6c18569a93
Rene Meusel <rene@renemeusel.de>
2013-11-26 16:14:04 +0000

    remove automatic status updates

--- web/stratum0/urls.py
+++ web/stratum0/urls.py
@@ -5,6 +5,6 @@ from stratum0 import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^(?P<stratum0_fqrn>.+)/replicate/(?P<stratum1_id>\d+)/$', views.StartReplicationRedirectView.as_view(), name='replicate'),
-    url(r'^(?P<stratum0_fqrn>.+)/most_recent/$', views.details, {'most_recent': True}, name='details_most_recent'),
+    url(r'^(?P<stratum0_fqrn>.+)/stratum1_details/(?P<stratum1_id>\d+)/$', views.stratum1_details, name='stratum1_details'),
     url(r'^(?P<stratum0_fqrn>.+)/$', views.details, name='details'),
 )
--- web/stratum0/views.py
+++ web/stratum0/views.py
@@ -33,18 +33,13 @@ def _find_most_recent(stratum0, stratum1s):
 
 
 @never_cache
-def details(request, stratum0_fqrn, most_recent=False):
+def details(request, stratum0_fqrn):
     try:
         stratum0  = cvmfs.repository.LocalRepository(stratum0_fqrn)
-        stratum1s = [ (stratum1, cvmfs.repository.RemoteRepository(stratum1.url)) for stratum1 in
-                      Stratum1.objects.filter(stratum0_fqrn=stratum0_fqrn) ]
+        stratum1s = Stratum1.objects.filter(stratum0_fqrn=stratum0_fqrn)
         context   = { 'stratum0'      : stratum0,
-                      'stratum1_list' : stratum1s,
-                      'most_recent'   : _find_most_recent(stratum0, stratum1s) }
-        if most_recent:
-            return render(request, 'stratum0/details_ajax.json', context, content_type="application/json")
-        else:
-            return render(request, 'stratum0/details.html', context)
+                      'stratum1_list' : stratum1s }
+        return render(request, 'stratum0/details.html', context)
     except cvmfs.repository.RepositoryNotFound, e:
         raise Http404
 
