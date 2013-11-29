8ca17204db2aa09254d8bec354ff3f940448f91d
Rene Meusel <rene@renemeusel.de>
2013-11-29 15:28:45 +0000

    factor out common behaviour in javascript

---
+++ web/cvmfs_web/static/js/base.js
@@ -0,0 +1,103 @@
+
+function _mangle_status_image_name(img_name, small) {
+    var result = ""
+    if (small) {
+        var tokens = img_name.match(/^(.*)(\.[a-z]{3})$/);
+        var base   = tokens[1];
+        var ext    = tokens[2];
+        result += base + "_small" + ext;
+    } else {
+        result = img_name;
+    }
+    return result;
+}
+
+function determine_replication_state_image(stratum1_json) {
+    var small = (arguments.length == 2) ? arguments[1] : false;
+    var img_name = "fail.png";
+
+    if (stratum1_json.status != 'ok') {
+        return _mangle_status_image_name("fail.png", small);
+    }
+
+    if (stratum1_json.replicating == "True") {
+        return _mangle_status_image_name("spinner.gif", small);
+    }
+
+    if (stratum1_json.revision == stratum1_json.stratum0_revision) {
+        return _mangle_status_image_name("tick.png", small);
+    }
+
+    if (stratum1_json.revision + 1 == stratum1_json.stratum0_revision) {
+        return _mangle_status_image_name("tick_degraded.png", small);
+    }
+
+    return _mangle_status_image_name("cross.png", small);
+}
+
+function compare_json(j1, j2) {
+    var k1 = Object.keys(j1).sort();
+    var k2 = Object.keys(j2).sort();
+    if (k1.length != k2.length) return false;
+    for (var i = 0; i < k1.length; ++i) {
+        if (k1[i] != k2[i]) return false;                        // key name
+        if (typeof j1[k1[i]] != typeof j2[k2[i]]) return false;  // value type
+        if (typeof j1[k1[i]] == 'object') {                      // recursion
+            if(!compare_json(j1[k1[i]],j2[k2[i]])) return false; // deep compare
+        }
+        else if (j1[k1[i]] != j2[k2[i]]) return false;           // value
+    }
+    return true;
+}
+
+function update_retina_image(img_object) {
+    var is_retina = img_object.attr("src").match(/^(.*)@2x(\..*)$/);
+    if (is_retina) {
+        img_object.attr("src", is_retina[1] + is_retina[2]);
+    }
+    img_object.attr("width", "");
+    img_object.attr("height", "");
+    new RetinaImage(img_object[0])
+}
+
+function replace_image(img_object, new_img_filename) {
+    var base = img_object.attr("src").match(/([^\.]+\/)[^\/]+/)[1];
+    img_object.attr("src", base + new_img_filename)
+    update_retina_image(img_object);
+}
+
+function leaky_update(url, min_refresh_rate, user_data, success_clb, fail_clb) {
+    var old_json    = (arguments.length > 5) ? arguments[5] : null;
+    var water_drops = (arguments.length > 6) ? arguments[6] : 0;
+
+    $.getJSON(url)
+        .done(function(json_data) {
+            if (!old_json || !compare_json(old_json, json_data)) {
+                water_drops = 0;
+                success_clb(user_data, json_data);
+            } else {
+                ++water_drops;
+            }
+
+            var next_update = Math.min(1000 * Math.pow(2, water_drops),
+                                       1000 * min_refresh_rate);
+            setTimeout(function() {
+                           leaky_update(url,
+                                        min_refresh_rate,
+                                        user_data,
+                                        success_clb,
+                                        fail_clb,
+                                        json_data,
+                                        water_drops);
+                       }, next_update);
+        })
+
+        .fail(function(data) {
+            fail_clb(user_data, data);
+        });
+}
+
+function format_timestamp(iso_timestamp) {
+    var date_obj = new Date(iso_timestamp)
+    return date_obj.toLocaleDateString() + " " + date_obj.toLocaleTimeString();
+}
